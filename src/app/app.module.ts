0dfdb7d2217a8b3f6dc59591d9551872f2808617
Andreas Larssen <andreaslarssen@gmail.com>
2021-02-18 22:41:42 +0100

    ADD: demo button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
+import {FihButtonModule} from '../../projects/fih-button/src/lib/fih-button.module';
 import {FihLoadingIndicatorModule} from 'fih-loading-indicator';
 
 @NgModule({
@@ -9,6 +10,7 @@ import {FihLoadingIndicatorModule} from 'fih-loading-indicator';
   ],
   imports: [
     BrowserModule,
+    FihButtonModule,
     FihLoadingIndicatorModule
   ],
   providers: [],
