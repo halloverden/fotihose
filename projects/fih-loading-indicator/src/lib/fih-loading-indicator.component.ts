bccba39eb25c44190d9fd5207af25573a2a96a2f
Andreas Larssen <andreaslarssen@gmail.com>
2021-03-08 21:17:06 +0100

    ADD: handle input changes

--- projects/fih-loading-indicator/src/lib/fih-loading-indicator.component.ts
+++ projects/fih-loading-indicator/src/lib/fih-loading-indicator.component.ts
@@ -3,13 +3,13 @@ import {
   ChangeDetectionStrategy,
   Component,
   ElementRef,
-  Input,
-  Renderer2,
+  Input, OnChanges,
+  Renderer2, SimpleChanges,
   ViewChild
 } from '@angular/core';
 
 /**
- * @example <fih-loading-indicator size=15></fih-loading-indicator>
+ * @example <fih-loading-indicator size=15 color=#00></fih-loading-indicator>
  */
 @Component({
   selector: 'fih-loading-indicator',
@@ -17,7 +17,7 @@ import {
   styleUrls: ['./fih-loading-indicator.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FihLoadingIndicatorComponent implements AfterViewInit {
+export class FihLoadingIndicatorComponent implements AfterViewInit, OnChanges {
   @Input()
   color = '#000';
 
@@ -39,8 +39,37 @@ export class FihLoadingIndicatorComponent implements AfterViewInit {
    *
    */
   ngAfterViewInit(): void {
+    this._setSize();
+    this._setColor();
+  }
+
+  /**
+   *
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    // Color
+    if (changes.color && !changes.color.isFirstChange()) {
+      this._setColor();
+    }
+
+    // Size
+    if (changes.size && !changes.size.isFirstChange()) {
+      this._setSize();
+    }
+  }
+
+  /**
+   *
+   */
+  private _setColor(): void {
+    this._renderer.setStyle(this.spinnerWrapper.nativeElement, 'stroke', this.color);
+  }
+
+  /**
+   *
+   */
+  private _setSize(): void {
     this._renderer.setStyle(this.loadingIndicator.nativeElement, 'height', this.size + 'px');
     this._renderer.setStyle(this.loadingIndicator.nativeElement, 'width', this.size + 'px');
-    this._renderer.setStyle(this.spinnerWrapper.nativeElement, 'stroke', this.color);
   }
 }
