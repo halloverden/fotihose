de313e10a13817f356805f82000fb75bf94f6cf4
Andreas Larssen <andreaslarssen@gmail.com>
2021-06-03 23:46:08 +0200

    EDIT: handle width and remove recalculation of arrow position

--- projects/fih-tooltip/src/lib/fih-tooltip.component.ts
+++ projects/fih-tooltip/src/lib/fih-tooltip.component.ts
@@ -36,13 +36,10 @@ export class FihTooltipComponent implements OnInit, AfterViewInit {
   arrowPosition: 'bottom' | 'bottom-left' | 'bottom-right' | 'left' | 'right' | 'top-left' | 'top' | 'top-right' | 'none' | string;
 
   @Input()
-  recalculateArrow = false;
-
-  @Input()
   id: string;
 
   @Input()
-  minWidth = 200;
+  minWidth: number = undefined;
 
   @Input()
   show = false;
@@ -80,7 +77,9 @@ export class FihTooltipComponent implements OnInit, AfterViewInit {
       this._renderer.setStyle(this.tooltipArrow.nativeElement, 'width', this.width + 'px');
     }
 
-    this._renderer.setStyle(this.tooltipArrow.nativeElement, 'min-width', this.minWidth + 'px');
+    if (this.minWidth) {
+      this._renderer.setStyle(this.tooltipArrow.nativeElement, 'min-width', this.minWidth + 'px');
+    }
 
     if (this.show) {
       this.open();
@@ -131,102 +130,8 @@ export class FihTooltipComponent implements OnInit, AfterViewInit {
    *
    */
   open(): void {
-    if (this.arrowPosition && this.arrowPosition !== FfTooltipArrowPositions.none && this.recalculateArrow) {
-      this._positionWithinBounds();
-    }
-
     this.show = true;
     this._cdr.detectChanges();
-  }
-
-  /**
-   * Perform checks for whether a tooltip is out of bounds. Clockwise order
-   *
-   */
-  private _positionWithinBounds(): void {
-    const overflows = this._getOverflows();
-
-    switch (this.arrowPosition) {
-      case FfTooltipArrowPositions.top:
-        if (overflows.bottom) {
-          this.arrowPosition = FfTooltipArrowPositions.bottom;
-        }
-        break;
-
-      case FfTooltipArrowPositions.topRight:
-        if (overflows.bottom && overflows.left) {
-          this.arrowPosition = FfTooltipArrowPositions.bottomLeft;
-        } else if (overflows.left) {
-          this.arrowPosition = FfTooltipArrowPositions.topLeft;
-        } else if (overflows.bottom) {
-          this.arrowPosition = FfTooltipArrowPositions.bottomRight;
-        }
-        break;
-
-      case FfTooltipArrowPositions.right:
-        if (overflows.left) {
-          this.arrowPosition = FfTooltipArrowPositions.left;
-        }
-        break;
-
-      case FfTooltipArrowPositions.bottomRight:
-        if (overflows.top && overflows.left) {
-          this.arrowPosition = FfTooltipArrowPositions.topLeft;
-        } else if (overflows.left) {
-          this.arrowPosition = FfTooltipArrowPositions.bottomLeft;
-        } else if (overflows.top) {
-          this.arrowPosition = FfTooltipArrowPositions.topRight;
-        }
-        break;
-
-      case FfTooltipArrowPositions.bottom:
-        if (overflows.top) {
-          this.arrowPosition = FfTooltipArrowPositions.top;
-        }
-        break;
-
-      case FfTooltipArrowPositions.bottomLeft:
-        if (overflows.top && overflows.right) {
-          this.arrowPosition = FfTooltipArrowPositions.topRight;
-        } else if (overflows.right) {
-          this.arrowPosition = FfTooltipArrowPositions.bottomRight;
-        } else if (overflows.top) {
-          this.arrowPosition = FfTooltipArrowPositions.bottomRight;
-        }
-        break;
-
-      case FfTooltipArrowPositions.left:
-        if (overflows.right) {
-          this.arrowPosition = FfTooltipArrowPositions.right;
-        }
-        break;
-
-      case FfTooltipArrowPositions.topLeft:
-        if (overflows.bottom && overflows.right) {
-          this.arrowPosition = FfTooltipArrowPositions.bottomRight;
-        } else if (overflows.right) {
-          this.arrowPosition = FfTooltipArrowPositions.topRight;
-        } else if (overflows.bottom) {
-          this.arrowPosition = FfTooltipArrowPositions.bottomLeft;
-        }
-        break;
-    }
-  }
-
-  /**
-   *
-   */
-  private _getOverflows(): {top: boolean, right: boolean, bottom: boolean, left: boolean} {
-    const tipSize: DOMRect = this.tooltip.nativeElement.getBoundingClientRect();
-    const vh = this._window.innerHeight || 0;
-    const vw = this._window.innerWidth || 0;
-
-    return {
-      top: (tipSize.top < 0),
-      right: (tipSize.right > 0 && tipSize.right > vw),
-      bottom: (tipSize.bottom > 0 && tipSize.bottom > vh),
-      left: (tipSize.left > 0 && tipSize.left < 0)
-    };
   }
 
   /**
